90c4223a89c42411721ba634203c259798763ff0
Eve <99390776+eveoreveline@users.noreply.github.com>
2022-11-28 18:32:24 +0900

    Create airflow_setup_mac_m1.py

---
+++ airflow_setup_mac_m1.py
@@ -0,0 +1,8 @@
+# mac useradd command
+# if this doesn't work, just go to 'preference' (reference: https://support.apple.com/ko-kr/HT201548)
+# you can also set your password there!
+
+sudo dscl . -create /home_directory_name/username
+
+# mac user login command in terminal (not gui)
+login
